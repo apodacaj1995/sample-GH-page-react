124ef7629c448b9a353534eb92fb04cad3e67d8a
apodacaj1995 <jaclynkapodaca@gmail.com>
2020-08-06 16:59:18 -0600

    Updates

---
+++ precache-manifest.449c82a4453780f137658830ad20af70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3d2690afd5e4ed74c8399e4a59abcb",
+    "url": "/sample-GH-pages-react/index.html"
+  },
+  {
+    "revision": "2d682e2191eb63638c3e",
+    "url": "/sample-GH-pages-react/static/css/main.10856169.chunk.css"
+  },
+  {
+    "revision": "c743dcc7a7d635557717",
+    "url": "/sample-GH-pages-react/static/js/2.54ffdb41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample-GH-pages-react/static/js/2.54ffdb41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d682e2191eb63638c3e",
+    "url": "/sample-GH-pages-react/static/js/main.862e0a0e.chunk.js"
+  },
+  {
+    "revision": "56a94bfdf763681b40f0",
+    "url": "/sample-GH-pages-react/static/js/runtime-main.c6f2c391.js"
+  }
+]);
